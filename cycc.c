876a67c219ac4ee5049117ff26ac772f3fdcb8f9
cydry3 <ysaita09@gmail.com>
2021-01-04 10:28:50 +0900

    Add tokenizer

--- cycc.c
+++ cycc.c
@@ -1,6 +1,109 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;        // If kind is TK_NUM、then it's value.
+  char *str;      // A string of the token
+};
+
+// Current token
+Token *token;
+
+
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// If a next token is equel to expected one,
+// then consume a token and returns True,
+// Otherwise, returns False value.
+bool consume(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    return false;
+  token = token->next;
+  return true;
+}
+
+// If a next token is equel to expected one,
+// then consume a token and returns True,
+// Otherwise, reports error.
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error("expected '%c', but '%c'", op, token->str[0]);
+  token = token->next;
+}
+
+// If the next token type is a number, consume a token and returns the value.
+// Otherwise, reports error.
+int expect_number() {
+  if (token->kind != TK_NUM)
+    error("数ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() {
+  return token->kind == TK_EOF;
+}
+
+// Make a new token, add it to current token as the next token.
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+// tokenize input charactors, returns it.
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("cannot tokenize");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "The number of arguments is not correct\n");
