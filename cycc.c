50012e76fe20590288015ba2422bb88a5429b1c1
cydry3 <ysaita09@gmail.com>
2021-01-09 11:52:53 +0900

    Add AST nodes, functions for syntax

--- cycc.c
+++ cycc.c
@@ -5,6 +5,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Kinds of node for Abstract Ayntax Tree.
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // number
+} NodeKind;
+
+typedef struct Node Node;
+
+// A node for AST.
+struct Node {
+  NodeKind kind;
+  Node *lhs;     // left  hand side
+  Node *rhs;     // right hand side
+  int val;       // If kind is TK_NUM、then it's value.
+};
+
+// Syntaxes.
+Node *expr();
+Node *mul();
+Node *primary();
+
+
 typedef enum {
   TK_RESERVED,
   TK_NUM,
@@ -121,6 +146,57 @@ Token *tokenize(char *p) {
 
   new_token(TK_EOF, cur, p);
   return head.next;
+}
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  return new_node_num(expect_number());
 }
 
 int main(int argc, char **argv) {
