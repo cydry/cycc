e99587606a191fc3e1a519d20296ce79ed78a82d
cydry3 <ysaita09@gmail.com>
2021-04-20 08:13:05 +0900

    Rewrite all tests with calling function form.

--- test.sh
+++ test.sh
@@ -42,55 +42,55 @@ assert_stdout() {
   fi
 }
 
-assert 0 'main() {0;}'
-# assert 42 '42;'
-# assert 41 " 12 + 34 - 5 ;"
-# assert_fail "1+3++;"
-# #assert_fail "1 + foo + 5;"
-# assert 47 '5+6*7;'
-# assert 15 '5*(9-6);'
-# assert 4 '(3+5)/2;'
-# assert 10 '-10+20;'
+assert 0 'main(){0;}'
+assert 42 'main(){42;}'
+assert 41 "main(){ 12 + 34 - 5 ;}"
+assert_fail "main(){1+3++;}"
+#assert_fail "1 + foo + 5;"
+assert 47 'main(){5+6*7;}'
+assert 15 'main(){5*(9-6);}'
+assert 4 'main(){(3+5)/2;}'
+assert 10 'main(){-10+20;}'
 
-# assert 1 '2 == 2;'
-# assert 0 '2 == 3;'
-# assert 0 '2 != 2;'
-# assert 1 '2 != 3;'
+assert 1 'main(){2 == 2;}'
+assert 0 'main(){2 == 3;}'
+assert 0 'main(){2 != 2;}'
+assert 1 'main(){2 != 3;}'
 
-# assert 0 '3 <= 2;'
-# assert 1 '2 <= 2;'
-# assert 1 '2 <= 3;'
-# assert 0 '3 < 2;'
-# assert 0 '2 < 2;'
-# assert 1 '2 < 3;'
+assert 0 'main(){3 <= 2;}'
+assert 1 'main(){2 <= 2;}'
+assert 1 'main(){2 <= 3;}'
+assert 0 'main(){3 < 2;}'
+assert 0 'main(){2 < 2;}'
+assert 1 'main(){2 < 3;}'
 
-# assert 0 '2 >= 3;'
-# assert 1 '2 >= 2;'
-# assert 1 '3 >= 2;'
-# assert 0 '2 > 3;'
-# assert 0 '2 > 2;'
-# assert 1 '3 > 2;'
+assert 0 'main(){2 >= 3;}'
+assert 1 'main(){2 >= 2;}'
+assert 1 'main(){3 >= 2;}'
+assert 0 'main(){2 > 3;}'
+assert 0 'main(){2 > 2;}'
+assert 1 'main(){3 > 2;}'
 
 assert 1 'main(){a = 1;}'
-# assert 1 'a = 1; return a;'
-# assert 5 'foo = 5;'
-# assert 5 'foo = 5; return foo;'
+assert 1 'main(){a = 1; return a;}'
+assert 5 'main(){foo = 5;}'
+assert 5 'main(){foo = 5; return foo;}'
 
-# assert 5 'return 5;'
-# assert 8 'return 8;'
-# assert 5 'return 2 + 3;'
+assert 5 'main(){return 5;}'
+assert 8 'main(){return 8;}'
+assert 5 'main(){return 2 + 3;}'
 
-# assert 2 'if (1) return 2; return 1;'
-# assert 3 'if (0) return 1; else return 3; return 2;'
-# assert 1 'if (2>1) return 1; else return 3; return 2;'
-# assert 5 'if (2<1) return 1; else return 2 + 3; return 2;'
+assert 2 'main(){if (1) return 2; return 1;}'
+assert 3 'main(){if (0) return 1; else return 3; return 2;}'
+assert 1 'main(){if (2>1) return 1; else return 3; return 2;}'
+assert 5 'main(){if (2<1) return 1; else return 2 + 3; return 2;}'
 
-# assert 9 'a = 0; while(a<9) a = a + 1; return a;'
-# assert 18 'b = 0; for (a = 0; a<9; a = a + 1) b = b + 2; return b;'
+assert 9 'main(){a = 0; while(a<9) a = a + 1; return a;}'
+assert 18 'main(){b = 0; for (a = 0; a<9; a = a + 1) b = b + 2; return b;}'
 
-# assert 18 'a = 0; while(a<18) { a = a + 1; a = a + 1; } return a;'
-# assert 36 'b = 0; for (a = 0; a<9; a = a + 1) { b = b + 2; b = b + 2; } return b;'
-# assert 5  'a = 2<1; if (a) { return 1; } else { return 2 + 3; } return 2;'
+assert 18 'main(){a = 0; while(a<18) { a = a + 1; a = a + 1; } return a;}'
+assert 36 'main(){b = 0; for (a = 0; a<9; a = a + 1) { b = b + 2; b = b + 2; } return b;}'
+assert 5  'main(){a = 2<1; if (a) { return 1; } else { return 2 + 3; } return 2;}'
 
 assert_stdout 2 'main(){foo();}'
 assert_stdout 5 'main(){bar(2,3);}'
