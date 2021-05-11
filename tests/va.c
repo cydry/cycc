f645425add3569d143217b4124baf1ad2205365a
cydry3 <ysaita09@gmail.com>
2021-05-12 06:11:59 +0900

    Rename builtin va list.

--- tests/va.c
+++ tests/va.c
@@ -1,7 +1,6 @@
-#include "cycc.h"
 
-typedef __builtin_va_list __gnuc_va_list;
-typedef __gnuc_va_list va_list;
+typedef __builtin_va_list __cycc_va_list;
+typedef __cycc_va_list va_list;
 
 int main() {
   return 0;
