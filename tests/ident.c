d7442aa17dd85a267ed3f33f8c728f2e32221276
cydry3 <ysaita09@gmail.com>
2021-05-11 12:45:34 +0900

    Imporve tokenizer.

--- tests/ident.c
+++ tests/ident.c
@@ -3,9 +3,17 @@ int ident_has_score() {
   return u_score;
 }
 
+int __initial_has_score() {
+  int u_score = 3;
+  return u_score;
+}
+
 int main() {
   int got = 0;
   got = ident_has_score();
   assert(2, got);
+
+  got = __initial_has_score();
+  assert(3, got);
   return 0;
 }
--- tokenize.c
+++ tokenize.c
@@ -19,6 +19,13 @@ int is_alnum(char c) {
          (c == '_');
 }
 
+// Returns if alphabet or underscore.
+int is_alpha(char c) {
+  return ('a' <= c && c <= 'z') ||
+         ('A' <= c && c <= 'Z') ||
+         (c == '_');
+}
+
 
 // Tokenize input charactors, returns it.
 Token *tokenize(char *p) {
@@ -204,7 +211,7 @@ Token *tokenize(char *p) {
     }
 
     // Identifier
-    if (isalpha(*p)) {
+    if (is_alpha(*p)) {
       char* begin = p;
       while (is_alnum(*p))
 	p++;
